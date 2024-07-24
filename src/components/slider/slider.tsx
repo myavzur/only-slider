5f3ebfa09386b64836a6dc35dca98b4bb2b305ae
myavzur <shwave_id404@mail.ru>
2024-07-24 05:43:24 +0300

    Slider controls

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,27 +1,88 @@
 import cn from "classnames";
 import { SliderProps } from "./props.interface";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
 import "swiper/css";
 
-import { InnerNavigation } from "./inner-navigation";
 import styles from "./styles.module.scss";
+import { useCallback, useEffect, useRef } from "react";
+import { Icon } from "../icon";
 
 
 export const Slider = <T extends unknown>({ slides, children, className }: SliderProps<T>) => {
-	return (
-		<Swiper
-			className={cn(styles.slider, className)}
-			spaceBetween={80}
-			slidesPerView={3}
-			grabCursor={true}
-		>
-			{slides.map((slide, idx) => (
-				<SwiperSlide key={idx} className={styles.slider__slide}>
-					{children(slide)}
-				</SwiperSlide>
-			))}
+	const sliderEl = useRef<SwiperRef | null>(null);
+	const controlPrevEl = useRef<HTMLButtonElement | null>(null);
+	const controlNextEl = useRef<HTMLButtonElement | null>(null);
 
-			<InnerNavigation />
-		</Swiper>
+	const updateControls = useCallback(() => {
+		if (!sliderEl.current || !controlPrevEl.current || !controlNextEl.current) return;
+
+		const swiper = sliderEl.current.swiper;
+
+		if (swiper.isBeginning) {
+			controlPrevEl.current.setAttribute("disabled", "true");
+		} else {
+			controlPrevEl.current.removeAttribute("disabled");
+		}
+
+		if (swiper.isEnd) {
+			controlNextEl.current.setAttribute("disabled", "true");
+		} else {
+			controlNextEl.current.removeAttribute("disabled");
+		}
+	}, [])
+
+	const handleSlidePrev = useCallback(() => {
+		if (!sliderEl.current) return;
+
+		const swiper = sliderEl.current.swiper;
+		swiper.slidePrev();
+
+		updateControls();
+	}, [updateControls]);
+
+	const handleSlideNext = useCallback(() => {
+		if (!sliderEl.current) return;
+
+		const swiper = sliderEl.current.swiper;
+		swiper.slideNext();
+
+		updateControls();
+	}, [updateControls]);
+
+	useEffect(() => {
+		updateControls();
+	}, [slides, updateControls]);
+
+	return (
+		<div className={styles.slider}>
+			<Swiper
+				ref={sliderEl}
+				spaceBetween={80}
+				slidesPerView={3}
+				grabCursor={true}
+			>
+				{slides.map((slide, idx) => (
+					<SwiperSlide key={idx} className={styles.slider__slide}>
+						{children(slide)}
+					</SwiperSlide>
+				))}
+			</Swiper>
+
+			<button
+				ref={controlPrevEl}
+				className={cn(styles.slider__control, styles.slider__control_prev)}
+				onClick={handleSlidePrev}
+			>
+				<Icon icon="arrow-left" />
+			</button>
+
+			<button
+				ref={controlNextEl}
+				className={cn(styles.slider__control, styles.slider__control_next)}
+				onClick={handleSlideNext}
+			>
+				<Icon icon="arrow-left" />
+			</button>
+		</div>
 	);
 };
